cd5806368954f08aedd76376bd969f7144f477a6
Tream <amanaila@oakland.edu>
2017-11-21 14:43:49 -0500

    error fixing and added Tag class

---
+++ src/Tag.java
@@ -0,0 +1,18 @@
+
+//theworldisquiethere
+
+import java.io.File;
+
+public class Tag {
+
+	private String Name;
+	private File Folder;
+
+	public Tag(String container) { Name = Container; }
+
+	public Tag(String Container, File Container2) { Name = Container; Folder = Container2; }
+
+	
+
+}
+
