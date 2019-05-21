bc7fc9877bcd2110e020b576c9c8701e5252a892
Alex Manaila <amanaila@oakland.edu>
2019-05-20 22:39:05 -0400

    general program window class (a part of the old PicTag.java power)

---
+++ src/WindowBox.java
@@ -0,0 +1,15 @@
+
+import javax.swing.*;
+import java.util.ArrayList;
+
+public class WindowBox extends JFrame {
+
+	private static PicTag INSTANCE;
+	private WindowAtlas WindowMap;
+	private ArrayList<Zone> Zones;
+	private KeyAction KA1, KA2, KA3;
+	
+	public WindowBox(PicTag INST, Dimension S) {
+		
+	}
+}
